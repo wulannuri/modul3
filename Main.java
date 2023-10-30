a5c4f6f6f2be537fefa7edf4f95ac5d230cb5a2c
wulannuri <wulannuri0912@gmail.com>
2023-10-30 10:11:20 +0700

    first commit

---
+++ Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public void hitung(){
+        double panjang = 20;
+        double lebar = 10;
+        double rumus;
+        rumus = panjang + lebar;
+        System.out.println("keliling persegi panjang : "+rumus);
+    }
+    public static void main(String[] args) {
+        Main obj = new Main();
+        obj.hitung();
+    }
+}
